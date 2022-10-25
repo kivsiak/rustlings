3e339df8f11d53910a493d881f119329d9263a61
kivsiak <kivsiak@gmail.com>
2022-10-25 20:29:18 +0400

    standard library types finished

--- exercises/standard_library_types/box1.rs
+++ exercises/standard_library_types/box1.rs
@@ -16,11 +16,10 @@
 //
 // Execute `rustlings hint box1` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
 #[derive(PartialEq, Debug)]
 pub enum List {
-    Cons(i32, List),
+    Cons(i32, Box<List>),
     Nil,
 }
 
@@ -32,12 +31,13 @@ fn main() {
     );
 }
 
-pub fn create_empty_list() -> List {
-    todo!()
+pub fn create_empty_list() -> List
+{
+    List::Nil
 }
 
 pub fn create_non_empty_list() -> List {
-    todo!()
+    List::Cons(1, Box::from(List::Nil))
 }
 
 #[cfg(test)]
--- exercises/standard_library_types/iterators4.rs
+++ exercises/standard_library_types/iterators4.rs
@@ -1,18 +1,9 @@
 // iterators4.rs
 // Execute `rustlings hint iterators4` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
 pub fn factorial(num: u64) -> u64 {
-    // Complete this function to return the factorial of num
-    // Do not use:
-    // - return
-    // Try not to use:
-    // - imperative style loops (for, while)
-    // - additional variables
-    // For an extra challenge, don't use:
-    // - recursion
-    // Execute `rustlings hint iterators4` for hints.
+    (1..=num).fold(1, |mut acc, val| acc * val)
 }
 
 #[cfg(test)]
@@ -28,6 +19,7 @@ mod tests {
     fn factorial_of_1() {
         assert_eq!(1, factorial(1));
     }
+
     #[test]
     fn factorial_of_2() {
         assert_eq!(2, factorial(2));
